111c14fb63164bdf23bcc28c4e64300e83279201
Silviya Dimova <silvia.dimova@yahoo.com>
2021-05-30 12:30:23 +0100

    Home and Matches screens layouts

--- app/src/main/java/com/sniper/tennis/insight/home/HomeActivity.kt
+++ app/src/main/java/com/sniper/tennis/insight/home/HomeActivity.kt
@@ -1,7 +1,9 @@
 package com.sniper.tennis.insight.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.sniper.tennis.insight.R
 
 class HomeActivity: AppCompatActivity(), HomePresenter.View {
@@ -11,5 +13,15 @@ class HomeActivity: AppCompatActivity(), HomePresenter.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity_layout)
+        val matchesButton = findViewById<AppCompatButton>(R.id.home_matches_button)
+        matchesButton.setOnClickListener {
+            presenter.displayMatches()
+
+        }
+    }
+
+    override fun navigateTo(target: Class<*>) {
+        val matchesActivityIntent = Intent(this,target)
+        startActivity(matchesActivityIntent)
     }
 }
--- app/src/main/java/com/sniper/tennis/insight/home/HomePresenter.kt
+++ app/src/main/java/com/sniper/tennis/insight/home/HomePresenter.kt
@@ -1,9 +1,14 @@
 package com.sniper.tennis.insight.home
 
-class HomePresenter (private val view: View, private val model: HomeModel){
+import com.sniper.tennis.insight.matches.MatchesActivity
 
+class HomePresenter (private val view: View, private val model: HomeModel){
+    fun displayMatches() {
+        view.navigateTo(MatchesActivity::class.java)
+    }
 
     interface View{
+        fun navigateTo(target :Class<*>)
 
     }
 }
