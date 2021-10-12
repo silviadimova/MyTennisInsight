0c18836f1bf94e85d5722cd28d66885cf437a6d4
Stoicho Andreev <stoichoandreev@yahoo.com>
2021-10-12 13:11:59 +0100

    Adding some unit tests

--- app/src/main/java/com/sniper/tennis/insight/home/HomePresenter.kt
+++ app/src/main/java/com/sniper/tennis/insight/home/HomePresenter.kt
@@ -3,7 +3,10 @@ package com.sniper.tennis.insight.home
 import com.sniper.tennis.insight.match.MatchActivity
 import com.sniper.tennis.insight.matches.MatchesActivity
 
-class HomePresenter (private val view: View, private val model: HomeModel){
+class HomePresenter (
+    private val view: View,
+    private val model: HomeModel
+) {
     fun onMatchesClick() {
         view.navigateTo(MatchesActivity::class.java)
     }
